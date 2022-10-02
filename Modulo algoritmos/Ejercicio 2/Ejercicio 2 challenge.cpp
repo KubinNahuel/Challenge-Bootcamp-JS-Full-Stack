29e2c1abeb53a7d1e4f1bc46f447b2b5d8caf0c4
KubinNahuel <114875998+KubinNahuel@users.noreply.github.com>
2022-10-02 15:53:43 -0300

    Add files via upload

---
+++ Modulo algoritmos/Ejercicio 2/Ejercicio 2 challenge.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string c,i;
+	int g=0,k;
+	cout<<"Por favor establezca una contraseña: ";//se pide la contraseña
+	cin>>c;
+	cout<<endl;
+	for(int j=0;j<3;j++){
+		cout<<"ingrese su contraseña nuevamente: ";
+		cin>>i;
+			if(i==c){
+		cout<<endl<<"¡Felicitaciones, recordas tu contraseña! \nPresione una tecla para salir: ";//se la compara 
+		cin>>k;
+		break;
+			}
+			else{
+				g++;
+				if(g==3){
+					cout<<"¡Tenes que ejercitar la memoria! \nPresione una tecla para salir: ";//se la compara y dado el caso de que falle 3 veces, le dara error.
+					cin>>k;
+					break;
+				}
+			}
+	}
+	
+	return 0;
+}
+
